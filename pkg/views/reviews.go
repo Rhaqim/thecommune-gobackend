06227d2a711c84fcf9fc3ad84ed9895b968f93cd
Rhaqim <anusiemj@gmail.com>
2022-08-17 00:29:02 +0100

    removed if else statement for checking object present, BindJSON already checks, checked for errors after running query

--- pkg/views/reviews.go
+++ pkg/views/reviews.go
@@ -37,35 +37,35 @@ func GetRestaurantReviews(c *gin.Context) {
 		return
 	}
 
-	if request.ID == primitive.NilObjectID {
+	id, err := primitive.ObjectIDFromHex(request.ID.Hex())
+	config.CheckErr(err)
+
+	filter := bson.M{"restaurant_id": bson.M{"$ref": "RESTAURANTS", "$id": id}}
+
+	cursor, err := collection.Find(context.TODO(), filter)
+	if err != nil {
 		response.Type = "error"
-		response.Message = "id is required"
-		c.JSON(http.StatusBadRequest, response)
+		response.Message = "Error getting restaurant reviews"
+		c.JSON(http.StatusBadRequest, gin.H{"res": response})
 		return
-	} else {
-
-		id, err := primitive.ObjectIDFromHex(request.ID.Hex())
-		config.CheckErr(err)
-
-		filter := bson.M{"restaurant_id": bson.M{"$ref": "RESTAURANTS", "$id": id}}
-
-		cursor, err := collection.Find(context.TODO(), filter)
-		config.CheckErr(err)
-
-		for cursor.Next(context.TODO()) {
-			err := cursor.Decode(&restaurants)
-			config.CheckErr(err)
-
-			restaurant = append(restaurant, restaurants)
-
-		}
-
-		response.Type = "success"
-		response.Data = restaurant
-		response.Message = "Restaurants found"
-
-		c.JSON(http.StatusOK, response)
 	}
+
+	for cursor.Next(context.TODO()) {
+		err := cursor.Decode(&restaurants)
+		if err != nil {
+			log.Fatal(err)
+		}
+		config.CheckErr(err)
+
+		restaurant = append(restaurant, restaurants)
+
+	}
+
+	response.Type = "success"
+	response.Data = restaurant
+	response.Message = "Restaurants found"
+
+	c.JSON(http.StatusOK, response)
 }
 
 /*
@@ -86,41 +86,38 @@ func AddNewRestaurantReview(c *gin.Context) {
 		return
 	}
 	log.Println("request: ", request)
+	restaurant_id, err := primitive.ObjectIDFromHex(request.Restaurant_ID.Hex())
+	config.CheckErr(err)
 
-	if request.Review == "" || request.Restaurant_ID == primitive.NilObjectID || request.Reviewer == primitive.NilObjectID {
-		response.Type = "error"
-		response.Message = "name, review, rating, restaurant_id, user_id are required"
-		c.JSON(http.StatusBadRequest, response)
-		return
-	} else {
-		restaurant_id, err := primitive.ObjectIDFromHex(request.Restaurant_ID.Hex())
-		config.CheckErr(err)
+	user_id, err := primitive.ObjectIDFromHex(request.Reviewer.Hex())
+	config.CheckErr(err)
 
-		user_id, err := primitive.ObjectIDFromHex(request.Reviewer.Hex())
-		config.CheckErr(err)
-
-		request.Created_At = config.GetCurrentTime()
-		request.Updated_At = config.GetCurrentTime()
-		filter := bson.M{
-			"reviewer":      bson.M{"$ref": "USERS", "$id": user_id},
-			"review":        request.Review,
-			"rating":        request.Rating,
-			"spent":         request.Spent,
-			"review_images": request.Review_Images,
-			"restaurant_id": bson.M{"$ref": "RESTAURANTS", "$id": restaurant_id},
-			"dislike":       request.Dislike,
-			"like":          request.Like,
-			"created_at":    request.Created_At,
-			"updated_at":    request.Updated_At,
-		}
-		insertResult, err := collection.InsertOne(context.TODO(), filter)
-		config.CheckErr(err)
-		log.Println("insertResult: ", insertResult)
-		response.Type = "success"
-		response.Data = bson.M{"InsertID": insertResult.InsertedID.(primitive.ObjectID).Hex()}
-		response.Message = "Review added"
-		c.JSON(http.StatusOK, response)
+	request.Created_At = config.GetCurrentTime()
+	request.Updated_At = config.GetCurrentTime()
+	filter := bson.M{
+		"reviewer":      bson.M{"$ref": "USERS", "$id": user_id},
+		"review":        request.Review,
+		"rating":        request.Rating,
+		"spent":         request.Spent,
+		"review_images": request.Review_Images,
+		"restaurant_id": bson.M{"$ref": "RESTAURANTS", "$id": restaurant_id},
+		"dislike":       request.Dislike,
+		"like":          request.Like,
+		"created_at":    request.Created_At,
+		"updated_at":    request.Updated_At,
 	}
+	insertResult, err := collection.InsertOne(context.TODO(), filter)
+	if err != nil {
+		response.Type = "error"
+		response.Message = "Error adding new restaurant review"
+		c.JSON(http.StatusBadRequest, gin.H{"res": response})
+		return
+	}
+	log.Println("insertResult: ", insertResult)
+	response.Type = "success"
+	response.Data = bson.M{"InsertID": insertResult.InsertedID.(primitive.ObjectID).Hex()}
+	response.Message = "Review added"
+	c.JSON(http.StatusOK, response)
 }
 
 /*
@@ -142,21 +139,19 @@ func UpdateReviewLikeAndDislike(c *gin.Context) {
 		return
 	}
 	log.Println("request: ", request)
-	if request.ID == primitive.NilObjectID {
+	id, err := primitive.ObjectIDFromHex(request.ID.Hex())
+	config.CheckErr(err)
+	request.Updated_At = config.GetCurrentTime()
+	updateResult, err := collection.UpdateOne(context.TODO(), bson.M{"_id": id}, bson.M{"$set": bson.M{"like": request.Like, "dislike": request.Dislike, "updated_at": request.Updated_At}})
+	if err != nil {
 		response.Type = "error"
-		response.Message = "id is required"
-		c.JSON(http.StatusBadRequest, response)
+		response.Message = "Error updating review like and dislike"
+		c.JSON(http.StatusBadRequest, gin.H{"res": response})
 		return
-	} else {
-		id, err := primitive.ObjectIDFromHex(request.ID.Hex())
-		config.CheckErr(err)
-		request.Updated_At = config.GetCurrentTime()
-		updateResult, err := collection.UpdateOne(context.TODO(), bson.M{"_id": id}, bson.M{"$set": bson.M{"like": request.Like, "dislike": request.Dislike, "updated_at": request.Updated_At}})
-		config.CheckErr(err)
-		log.Println("updateResult: ", updateResult)
-		response.Type = "success"
-		response.Data = bson.M{"UpdateID": updateResult}
-		response.Message = "Review updated"
-		c.JSON(http.StatusOK, response)
 	}
+	log.Println("updateResult: ", updateResult)
+	response.Type = "success"
+	response.Data = bson.M{"UpdateID": updateResult}
+	response.Message = "Review updated"
+	c.JSON(http.StatusOK, response)
 }
