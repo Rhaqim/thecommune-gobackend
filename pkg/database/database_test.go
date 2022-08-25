bccb99800d9723427566971d309974c760852ad6
Rhaqim <anusiemj@gmail.com>
2022-08-25 02:47:42 +0100

    base tests

---
+++ pkg/database/database_test.go
@@ -0,0 +1,13 @@
+package database
+
+import (
+	"testing"
+)
+
+func TestConnectMongoDB(t *testing.T) {
+	t.Log("ConnectMongoDB")
+}
+
+func TestOpenCollection(t *testing.T) {
+	t.Log("OpenCollection")
+}
---
+++ pkg/views/views_test.go
@@ -0,0 +1,47 @@
+package views
+
+import "testing"
+
+func TestGetRestaurants(t *testing.T) {
+	t.Log("GetRestaurants")
+}
+
+func TestGetRestaurantByID(t *testing.T) {
+	t.Log("GetRestaurantByID")
+}
+
+func TestGetRestaurantReviews(t *testing.T) {
+	t.Log("GetRestaurantReviews")
+}
+
+func TestGetRestaurantByName(t *testing.T) {
+	t.Log("GetRestaurantByName")
+}
+
+func TestAddNewRestaurantReview(t *testing.T) {
+	t.Log("AddNewRestaurantReview")
+}
+
+func TestUpdateReviewLikeAndDislike(t *testing.T) {
+	t.Log("UpdateReviewLikeAndDislike")
+}
+
+func TestCreateRestaurant(t *testing.T) {
+	t.Log("CreateRestaurant")
+}
+
+func TestUpdateRestaurantAvgPrice(t *testing.T) {
+	t.Log("UpdateRestaurantAvgPrice")
+}
+
+func TestGetUserByID(t *testing.T) {
+	t.Log("GetUserByID")
+}
+
+func TestCreatNewUser(t *testing.T) {
+	t.Log("CreatNewUser")
+}
+
+func TestUpdateAvatar(t *testing.T) {
+	t.Log("UpdateAvatar")
+}
